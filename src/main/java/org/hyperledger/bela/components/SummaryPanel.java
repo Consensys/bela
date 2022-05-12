2e09811b8637c2ef3e880dfe1f1efa5db1aa45f5
Jiri Peinlich <jiri.peinlich@gmail.com>
2022-05-12 21:36:46 +0100

    Implementing keys using handlers
    Signed-off-by: Jiri Peinlich <jiri.peinlich@gmail.com>

--- src/main/java/org/hyperledger/bela/components/SummaryPanel.java
+++ src/main/java/org/hyperledger/bela/components/SummaryPanel.java
@@ -3,17 +3,20 @@ package org.hyperledger.bela.components;
 import com.googlecode.lanterna.gui2.GridLayout;
 import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.Panel;
-import org.hyperledger.besu.ethereum.core.Block;
-
-import java.util.Optional;
 
 public class SummaryPanel implements LanternaComponent<Panel>{
-  private final String worldStateRoot;
-  private final Optional<Block> block;
+  private final Label worldStateRoot = new Label("empty");
+  private final Label chainHead = new Label("empty");
+  private final Label chainHeadHash = new Label("empty");
 
-  public SummaryPanel(final String worldStateRoot, final Optional<Block> block) {
-    this.worldStateRoot = worldStateRoot;
-    this.block = block;
+  public SummaryPanel(final String worldStateRoot, final String chainHead, final String chainHeadhHash) {
+    updateWith(worldStateRoot, chainHead, chainHeadhHash);
+  }
+
+  public void updateWith(final String worldStateRoot, final String chainHead, final String chainHeadHash) {
+    this.worldStateRoot.setText(worldStateRoot);
+    this.chainHead.setText(chainHead);
+    this.chainHeadHash.setText(chainHeadHash);
   }
 
   @Override
@@ -22,13 +25,13 @@ public class SummaryPanel implements LanternaComponent<Panel>{
     panel.setLayoutManager(new GridLayout(2));
 
     panel.addComponent(new Label("db world state root:"));
-    panel.addComponent(new Label(worldStateRoot));
+    panel.addComponent(worldStateRoot);
 
     panel.addComponent(new Label("chain head:"));
-    panel.addComponent(new Label(String.valueOf(block.get().getHeader().getNumber())));
+    panel.addComponent(chainHead);
 
     panel.addComponent(new Label("chain head hash:"));
-    panel.addComponent(new Label(block.get().getHash().toHexString()));
+    panel.addComponent(chainHeadHash);
 
 
     return panel;
--- src/main/java/org/hyperledger/bela/windows/BlockChainBrowserWindow.java
+++ src/main/java/org/hyperledger/bela/windows/BlockChainBrowserWindow.java
@@ -4,11 +4,21 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.gui2.BasicWindow;
 import com.googlecode.lanterna.gui2.BorderLayout;
 import com.googlecode.lanterna.gui2.Borders;
+import com.googlecode.lanterna.gui2.Direction;
+import com.googlecode.lanterna.gui2.Label;
+import com.googlecode.lanterna.gui2.LinearLayout;
 import com.googlecode.lanterna.gui2.Panel;
 import com.googlecode.lanterna.gui2.Window;
+import com.googlecode.lanterna.gui2.WindowListener;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import org.hyperledger.bela.BlockChainBrowser;
 import org.hyperledger.bela.config.BelaConfigurationImpl;
 import org.hyperledger.besu.ethereum.storage.StorageProvider;
@@ -19,12 +29,17 @@ import org.hyperledger.besu.plugin.services.storage.rocksdb.RocksDBKeyValueStora
 import org.hyperledger.besu.plugin.services.storage.rocksdb.RocksDBMetricsFactory;
 import org.hyperledger.besu.plugin.services.storage.rocksdb.configuration.RocksDBCLIOptions;
 import org.hyperledger.besu.plugin.services.storage.rocksdb.configuration.RocksDBFactoryConfiguration;
+import org.jetbrains.annotations.NotNull;
 
-public class BlockChainBrowserWindow implements LanternaWindow {
+public class BlockChainBrowserWindow implements LanternaWindow, WindowListener {
+
+    private static final String[] PREV_NEXT_BLOCK_COMMANDS = {"prev Block", "'<-'", "next Block", "'->'", "Close", "'c'"};
 
     private ConfigWindow config;
     private BlockChainBrowser browser;
     private StorageProvider provider;
+    private BasicWindow window;
+    private Panel panel;
 
     public BlockChainBrowserWindow(final ConfigWindow config) {
 
@@ -56,20 +71,28 @@ public class BlockChainBrowserWindow implements LanternaWindow {
 
         // Create window to hold the panel
 
-        BasicWindow window = new BasicWindow("Bela DB Browser");
+        window = new BasicWindow("Bela DB Browser");
         window.setHints(List.of(Window.Hint.FULL_SCREEN));
-        Panel panel = new Panel(new BorderLayout());
 
-        var blockPanelHolder = new Panel();
+        panel = new Panel(new LinearLayout());
+
+        Panel commands = getCommandsPanel(PREV_NEXT_BLOCK_COMMANDS);
+
+        // add possible actions
+        panel.addComponent(commands);
 
         // add summary panel
         panel.addComponent(browser.showSummaryPanel().createComponent()
-                .withBorder(Borders.singleLine()), BorderLayout.Location.TOP);
+                .withBorder(Borders.singleLine()));
 
         // add block detail panel
-        blockPanelHolder.addComponent(browser.blockPanel().createComponent());
-        panel.addComponent(blockPanelHolder, BorderLayout.Location.BOTTOM);
+        panel.addComponent(browser.blockPanel().createComponent());
 
+        window.addWindowListener(this);
+        window.handleInput(new KeyStroke(KeyType.Escape));
+        window.handleInput(new KeyStroke(KeyType.ArrowLeft));
+        window.handleInput(new KeyStroke(KeyType.ArrowRight));
+        window.handleInput(new KeyStroke('c',false,false,false));
         window.setComponent(panel);
         return window;
     }
@@ -91,5 +114,64 @@ public class BlockChainBrowserWindow implements LanternaWindow {
                 .withCommonConfiguration(new BelaConfigurationImpl(dataDir, dbDir))
                 .withMetricsSystem(new NoOpMetricsSystem())
                 .build();
+    }
+
+    @NotNull
+    private static Panel getCommandsPanel(final String[] strings) {
+        Panel commands = new Panel(new LinearLayout(Direction.HORIZONTAL));
+        Panel key = new Panel(new LinearLayout());
+        key.addComponent(new Label("action").addStyle(SGR.BOLD));
+        key.addComponent(new Label("key").addStyle(SGR.BOLD));
+        commands.addComponent(key.withBorder(Borders.singleLine()));
+
+        int i = 0;
+        while (i < strings.length) {
+            Panel a = new Panel(new LinearLayout());
+            a.addComponent(new Label(strings[i++]).setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Center, LinearLayout.GrowPolicy.None)));
+            a.addComponent(new Label(strings[i++]).setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Center, LinearLayout.GrowPolicy.None)));
+            commands.addComponent(a.withBorder(Borders.singleLine()));
+        }
+        return commands;
+    }
+
+    @Override
+    public void onResized(final Window window, final TerminalSize oldSize, final TerminalSize newSize) {
+
+    }
+
+    @Override
+    public void onMoved(final Window window, final TerminalPosition oldPosition, final TerminalPosition newPosition) {
+
+    }
+
+    @Override
+    public void onInput(final Window basePane, final KeyStroke keyStroke, final AtomicBoolean deliverEvent) {
+        switch(keyStroke.getKeyType()) {
+            case ArrowLeft:
+                browser = browser.moveBackward();
+                break;
+
+            case ArrowRight:
+                browser = browser.moveForward();
+                break;
+
+            case Escape:
+                window.close();
+                break;
+            case Character:
+                switch (keyStroke.getCharacter()) {
+                    case 'c':
+                        window.close();
+                        break;
+                    default:
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
+    public void onUnhandledInput(final Window basePane, final KeyStroke keyStroke, final AtomicBoolean hasBeenHandled) {
+
     }
 }

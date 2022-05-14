fe870fedabee76d41d8ab0a4e10fe9626064d6ac
Jiri Peinlich <jiri.peinlich@gmail.com>
2022-05-14 11:08:58 +0100

    Overriding storage path is now disabled by default
    Signed-off-by: Jiri Peinlich <jiri.peinlich@gmail.com>

--- src/main/java/org/hyperledger/bela/windows/Constants.java
+++ src/main/java/org/hyperledger/bela/windows/Constants.java
@@ -5,7 +5,8 @@ public class Constants {
     public static final String DATA_PATH = "DATA_PATH";
     public static final String DATA_PATH_DEFAULT = ".";
     public static final String STORAGE_PATH = "STORAGE_PATH";
-    public static final String STORAGE_PATH_DEFAULT = "./database";
+    public static final String OVERRIDE_STORAGE_PATH = "OVERRIDE_STORAGE_PATH";
+    public static final String STORAGE_PATH_DEFAULT = "database";
     public static final String GENESIS_PATH = "GENESIS_PATH";
     public static final String GENESIS_PATH_DEFAULT = "";
     public static final String NODE_KEY = "NODE_KEY";

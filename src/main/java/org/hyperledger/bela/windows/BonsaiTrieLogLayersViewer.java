4e83819efba57280dbe139824bb4abf306d6871e
Jiri Peinlich <jiri.peinlich@gmail.com>
2022-07-18 14:05:28 +0200

    Adding roll forward/backward options

--- src/main/java/org/hyperledger/bela/windows/BonsaiTrieLogLayersViewer.java
+++ src/main/java/org/hyperledger/bela/windows/BonsaiTrieLogLayersViewer.java
@@ -2,22 +2,17 @@ package org.hyperledger.bela.windows;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import com.googlecode.lanterna.gui2.BasicWindow;
-import com.googlecode.lanterna.gui2.Component;
 import com.googlecode.lanterna.gui2.Direction;
 import com.googlecode.lanterna.gui2.EmptySpace;
-import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.LinearLayout;
 import com.googlecode.lanterna.gui2.Panel;
 import com.googlecode.lanterna.gui2.Window;
 import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
 import com.googlecode.lanterna.gui2.dialogs.TextInputDialog;
 import kr.pe.kwonnam.slf4jlambda.LambdaLogger;
-import org.apache.tuweni.bytes.Bytes;
-import org.apache.tuweni.bytes.Bytes32;
 import org.hyperledger.bela.components.KeyControls;
 import org.hyperledger.bela.components.bonsai.BonsaiTrieLogView;
 import org.hyperledger.bela.dialogs.BelaExceptionDialog;
@@ -25,10 +20,17 @@ import org.hyperledger.bela.utils.BlockChainContext;
 import org.hyperledger.bela.utils.BlockChainContextFactory;
 import org.hyperledger.bela.utils.StorageProviderFactory;
 import org.hyperledger.besu.datatypes.Hash;
+import org.hyperledger.besu.ethereum.bonsai.BonsaiPersistedWorldState;
+import org.hyperledger.besu.ethereum.bonsai.BonsaiWorldStateArchive;
+import org.hyperledger.besu.ethereum.bonsai.BonsaiWorldStateKeyValueStorage;
+import org.hyperledger.besu.ethereum.bonsai.BonsaiWorldStateUpdater;
 import org.hyperledger.besu.ethereum.bonsai.TrieLogLayer;
 import org.hyperledger.besu.ethereum.chain.ChainHead;
+import org.hyperledger.besu.ethereum.chain.DefaultBlockchain;
 import org.hyperledger.besu.ethereum.storage.StorageProvider;
 import org.hyperledger.besu.ethereum.storage.keyvalue.KeyValueSegmentIdentifier;
+import org.hyperledger.besu.ethereum.storage.keyvalue.KeyValueStoragePrefixedKeyBlockchainStorage;
+import org.hyperledger.besu.metrics.noop.NoOpMetricsSystem;
 import org.hyperledger.besu.plugin.services.storage.KeyValueStorage;
 
 import static kr.pe.kwonnam.slf4jlambda.LambdaLoggerFactory.getLogger;
@@ -36,13 +38,15 @@ import static org.hyperledger.bela.windows.Constants.KEY_CLOSE;
 import static org.hyperledger.bela.windows.Constants.KEY_FOCUS;
 import static org.hyperledger.bela.windows.Constants.KEY_HEAD;
 import static org.hyperledger.bela.windows.Constants.KEY_LOOKUP_BY_HASH;
+import static org.hyperledger.bela.windows.Constants.KEY_ROLL_BACKWARD;
+import static org.hyperledger.bela.windows.Constants.KEY_ROLL_FORWARD;
 
 public class BonsaiTrieLogLayersViewer implements BelaWindow {
     private static final LambdaLogger log = getLogger(BonsaiTrieLogLayersViewer.class);
 
     private final WindowBasedTextGUI gui;
     private final StorageProviderFactory storageProviderFactory;
-    private ArrayList<BonsaiTrieLogView> children = new ArrayList<>();
+    private BonsaiTrieLogView view;
     private final Panel triesPanel = new Panel();
 
     public BonsaiTrieLogLayersViewer(final WindowBasedTextGUI gui, final StorageProviderFactory storageProviderFactory) {
@@ -71,6 +75,8 @@ public class BonsaiTrieLogLayersViewer implements BelaWindow {
                 .addControl("Focus", KEY_FOCUS, this::checkFocus)
                 .addControl("By Hash", KEY_LOOKUP_BY_HASH, this::lookupByHash)
                 .addControl("Head", KEY_HEAD, this::lookupByChainHead)
+                .addControl("Roll Forward", KEY_ROLL_FORWARD, this::rollForward)
+                .addControl("Roll Backward", KEY_ROLL_BACKWARD, this::rollBackward)
                 .addControl("Close", KEY_CLOSE, window::close);
         window.addWindowListener(controls);
         panel.addComponent(controls.createComponent());
@@ -82,6 +88,37 @@ public class BonsaiTrieLogLayersViewer implements BelaWindow {
 
         window.setComponent(panel);
         return window;
+    }
+
+    private void rollBackward() {
+        final BonsaiWorldStateUpdater updater = getBonsaiWorldStateUpdater();
+        updater.rollBack(view.getLayer());
+        updater.commit();
+
+    }
+
+    private BonsaiWorldStateUpdater getBonsaiWorldStateUpdater() {
+        final StorageProvider provider = storageProviderFactory.createProvider();
+        final BlockChainContext blockChainContext = BlockChainContextFactory.createBlockChainContext(provider);
+
+        final BonsaiWorldStateArchive archive = new BonsaiWorldStateArchive(provider, blockChainContext.getBlockchain());
+        final BonsaiPersistedWorldState bonsaiState =
+                new BonsaiPersistedWorldState(
+                        archive,
+                        new BonsaiWorldStateKeyValueStorage(
+                                provider.getStorageBySegmentIdentifier(KeyValueSegmentIdentifier.ACCOUNT_INFO_STATE),
+                                provider.getStorageBySegmentIdentifier(KeyValueSegmentIdentifier.CODE_STORAGE),
+                                provider.getStorageBySegmentIdentifier(KeyValueSegmentIdentifier.ACCOUNT_STORAGE_STORAGE),
+                                provider.getStorageBySegmentIdentifier(KeyValueSegmentIdentifier.TRIE_BRANCH_STORAGE),
+                                provider.getStorageBySegmentIdentifier(KeyValueSegmentIdentifier.TRIE_LOG_STORAGE)));
+        return (BonsaiWorldStateUpdater) bonsaiState.updater();
+
+    }
+
+    private void rollForward() {
+        final BonsaiWorldStateUpdater updater = getBonsaiWorldStateUpdater();
+        updater.rollForward(view.getLayer());
+        updater.commit();
     }
 
     private void lookupByChainHead() {
@@ -114,17 +151,16 @@ public class BonsaiTrieLogLayersViewer implements BelaWindow {
         if (trieLog.isPresent()) {
             triesPanel.removeAllComponents();
             final BonsaiTrieLogView bonsaiTrieLogView = new BonsaiTrieLogView(hash, trieLog.get(), 0);
-            triesPanel.addComponent( bonsaiTrieLogView.createComponent());
-            children.clear();
-            children.add(bonsaiTrieLogView);
+            triesPanel.addComponent(bonsaiTrieLogView.createComponent());
+            view = bonsaiTrieLogView;
         } else {
             log.error("Trie log not found for hash: {}", hash);
         }
     }
 
     private void checkFocus() {
-        for (BonsaiTrieLogView child : children) {
-            child.focus();
+        if (view != null) {
+            view.focus();
         }
     }
 

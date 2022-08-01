d012e5bdf040555e2b93e31f7c30a7124c3f4463
Jiri Peinlich <jiri.peinlich@gmail.com>
2022-08-01 10:19:20 +0200

    moved things into bela context
    Signed-off-by: Jiri Peinlich <jiri.peinlich@gmail.com>

---
+++ src/main/java/org/hyperledger/bela/context/NodeKeyUtils.java
@@ -0,0 +1,27 @@
+package org.hyperledger.bela.context;
+
+import org.apache.tuweni.bytes.Bytes32;
+import org.hyperledger.besu.crypto.KeyPair;
+import org.hyperledger.besu.crypto.KeyPairSecurityModule;
+import org.hyperledger.besu.crypto.NodeKey;
+import org.hyperledger.besu.crypto.SignatureAlgorithm;
+import org.hyperledger.besu.crypto.SignatureAlgorithmFactory;
+
+class NodeKeyUtils {
+
+    public static NodeKey createFrom(final KeyPair keyPair) {
+        return new NodeKey(new KeyPairSecurityModule(keyPair));
+    }
+
+    public static NodeKey createFrom(final Bytes32 privateKey) {
+        final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithmFactory.getInstance();
+        final KeyPair keyPair =
+                signatureAlgorithm.createKeyPair(signatureAlgorithm.createPrivateKey(privateKey));
+        return new NodeKey(new KeyPairSecurityModule(keyPair));
+    }
+
+    public static NodeKey generate() {
+        return new NodeKey(
+                new KeyPairSecurityModule(SignatureAlgorithmFactory.getInstance().generateKeyPair()));
+    }
+}

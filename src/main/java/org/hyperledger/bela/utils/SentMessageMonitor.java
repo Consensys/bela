0bebe3b3ec11d85d11c55ef0f2bac105a4958bdf
Jiri Peinlich <jiri.peinlich@gmail.com>
2022-08-17 13:50:55 +0100

    Storing outgoing messages
    Signed-off-by: Jiri Peinlich <jiri.peinlich@gmail.com>

---
+++ src/main/java/org/hyperledger/bela/utils/SentMessageMonitor.java
@@ -0,0 +1,27 @@
+package org.hyperledger.bela.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.hyperledger.besu.ethereum.p2p.peers.Peer;
+import org.hyperledger.besu.ethereum.p2p.rlpx.wire.Capability;
+import org.hyperledger.besu.ethereum.p2p.rlpx.wire.MessageData;
+
+public class SentMessageMonitor {
+
+    private static final SentMessageMonitor instance = new SentMessageMonitor();
+    private final List<SentMessageSubscriber> subscribers = new ArrayList<>();
+
+    public static SentMessageMonitor getInstance() {
+        return instance;
+    }
+
+    public void subscribe(final SentMessageSubscriber subscriber) {
+        this.subscribers.add(subscriber);
+    }
+
+    public void message(final Peer peer, final Capability capability, final MessageData messageData) {
+        for (final SentMessageSubscriber subscriber : subscribers) {
+            subscriber.onSentMessage(peer, capability, messageData);
+        }
+    }
+}

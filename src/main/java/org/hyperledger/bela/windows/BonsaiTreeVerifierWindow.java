340d5273919a50e19b51104cdfb1cefba2a7eced
Jiri Peinlich <jiri.peinlich@gmail.com>
2022-09-12 15:13:19 +0100

    logging verification to logs
    Signed-off-by: Jiri Peinlich <jiri.peinlich@gmail.com>

--- src/main/java/org/hyperledger/bela/windows/BonsaiTreeVerifierWindow.java
+++ src/main/java/org/hyperledger/bela/windows/BonsaiTreeVerifierWindow.java
@@ -104,17 +104,19 @@ public class BonsaiTreeVerifierWindow extends AbstractBelaWindow implements Bons
 
     @Override
     public void missingCodeHash(final Hash codeHash, final Hash accountHash) {
+        log.info("Missing code hash {} for account {}", codeHash, accountHash);
         logTextBox.addLine(String.format("missing code hash %s for account %s", codeHash, accountHash));
-
     }
 
     @Override
     public void invalidCode(final Hash accountHash, final Hash codeHash, final Hash foundCodeHash) {
+        log.info("Invalid code for account {} with code hash {} found code hash {}", accountHash, codeHash, foundCodeHash);
         logTextBox.addLine(String.format("invalid code for account %s (expected %s and found %s)", accountHash, codeHash, foundCodeHash));
     }
 
     @Override
     public void missingValueForNode(final Bytes32 hash) {
+        log.info("Missing value for node {}", hash);
         logTextBox.addLine("Missing value for node " + hash.toHexString());
 
     }
@@ -127,32 +129,38 @@ public class BonsaiTreeVerifierWindow extends AbstractBelaWindow implements Bons
 
     @Override
     public void missingAccountTrieForHash(final Bytes32 hash, final Bytes location) {
+        log.info("Missing account trie for hash {} at location {}", hash, location);
         logTextBox.addLine(String.format("missing account trie node for hash %s and location %s", hash, location));
 
     }
 
     @Override
     public void invalidAccountTrieForHash(final Bytes32 hash, final Bytes location, final Hash foundHashNode) {
+        log.info("Invalid account trie for hash {} at location {} found hash {}", hash, location, foundHashNode);
         logTextBox.addLine(String.format("invalid account trie node for hash %s and location %s (found %s)", hash, location, foundHashNode));
     }
 
     @Override
     public void missingStorageTrieForHash(final Bytes32 hash, final Bytes location) {
+        log.info("Missing storage trie for hash {} at location {}", hash, location);
         logTextBox.addLine(String.format("missing storage trie node for hash %s and location %s", hash, location));
     }
 
     @Override
     public void invalidStorageTrieForHash(final Bytes32 hash, final Bytes location, final Hash foundHashNode) {
+        log.info("Invalid storage trie for hash {} at location {} found hash {}", hash, location, foundHashNode);
         logTextBox.addLine(String.format("invalid storage trie node for hash %s and location %s (found %s)", hash, location, foundHashNode));
     }
 
     @Override
     public void differentDataInFlatDatabaseForAccount(final Hash accountHash) {
+        log.info("Different data in flat database for account {}", accountHash);
         logTextBox.addLine(String.format("inconsistent data in flat database for account %s", accountHash));
     }
 
     @Override
     public void differentDataInFlatDatabaseForStorage(final Bytes32 accountHash, final Bytes32 slotHash) {
+        log.info("Different data in flat database for account {} and slot {}", accountHash, slotHash);
         logTextBox.addLine(String.format("inconsistent data in flat database for account %s on slot %s", accountHash, slotHash));
     }
 }
